b0bc9671cbc628b0018d70cf25b7bb5c94ff73e9
Brett Anderson <banderson@etereman.com>
2025-01-10 15:46:43 -0600

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -58,10 +58,7 @@ ASGI_APPLICATION = 'backend.asgi.application'
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
     },
 }
 
