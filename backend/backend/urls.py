2a6ca31bc6a59888400fbc28b2f2f28d3c9d8146
Brett Anderson <banderson@etereman.com>
2025-01-10 17:13:00 -0600

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/voting/', include('voting.urls')),  # Include voting app URLs
+    path('api/voting/', include('backend.voting.urls')),  # Include voting app URLs
 ]
